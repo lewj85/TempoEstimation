2c0ea318b13d5641dff1358055e81c871870c36b
Jesse Lew <jesse.lew@g.austincc.edu>
2018-04-23 22:01:46 -0400

    Divided data in training.py, added if-elseif for dataset in train.py

--- model/training.py
+++ model/training.py
@@ -1,19 +1,42 @@
 from keras.optimizers import Adam
 from model.architectures import construct_spectrogram_bilstm
+import numpy as np
 
-
-def train_model(X, y, model_type, lr=0.0001, batch_size=5, num_epochs=10):
+def train_model(X, y, model_type, lr=0.0001, batch_size=5, num_epochs=10, valid_ratio=0.2, test_ratio=0.2):
     # create BiLSTM and dense
     if model_type == 'spectrogram':
         model = construct_spectrogram_bilstm(X[0].shape[1])
     else:
         raise ValueError('Unsupported model type: {}'.format(model_type))
 
-    # create computational graph
+    # Create computational graph
     adm = Adam(lr=lr)
     model.compile(loss='categorical_crossentropy', optimizer=adm)
 
-    # training
-    model.fit(x=X, y=y, epochs=num_epochs, batch_size=batch_size)
+    # Divide data
+    n_examples = X.shape[0]
+    # Get random order
+    idx = np.random.permutation(n_examples)
+    
+    num_train = int(n_examples*(1-valid_ratio-test_ratio))
+    num_valid = int(n_examples*valid_ratio)
+    num_test = n_examples - num_train - num_valid
+    
+    train_idx = idx[:num_train]
+    valid_idx = idx[num_train:num_train+num_valid]
+    test_idx = idx[num_train+num_valid:]
+    
+    x = X[train_idx]
+    y = Y[train_idx]
+    x_valid = X[valid_idx]
+    y_valid = y[valid_idx]
+    x_test = X[test_idx]
+    y_test = y[test_idx]
+    
+    # Training
+    model.fit(x=X, y=y, validation_data=(x_valid, y_valid), epochs=num_epochs, batch_size=batch_size)
+    
+    # Run on test set
+    model.predict(x_test)
 
     return model
--- train.py
+++ train.py
@@ -17,10 +17,14 @@ def parse_arguments():
     parser.add_argument('label_dir', help='Path to annotation directory')
     parser.add_argument('dataset', choices=['hainsworth', 'ballroom'],
                         help='Name of dataset')
+    # -m is shortcut for --model
     parser.add_argument('--model', '-m', dest='model_type', default='spectrogram',
                         choices=['spectrogram', 'audio'], help='Model type')
 
+
+    # Creates a namespace object
     args = parser.parse_args()
+    # vars creates a dictionary where key:value pairs attribute name:attribute
     return vars(args)
 
 
@@ -29,8 +33,10 @@ def main(data_dir, label_dir, dataset, model_type='spectrogram'):
     Train a deep beat tracker model
     """
     # Load audio and annotations
-    a, r = prep_hainsworth_data(data_dir, label_dir, TARGET_FS)
-    #a, r = prep_ballroom_data(data_dir, label_dir, HOP_SIZE, TARGET_FS)
+    if dataset == 'hainsworth':
+        a, r = prep_hainsworth_data(data_dir, label_dir, TARGET_FS)
+    elif dataset == 'ballroom':
+        a, r = prep_ballroom_data(data_dir, label_dir, HOP_SIZE, TARGET_FS)
 
     # Get features and targets from data
     X, y = preprocess_data(a, r, mode=model_type)
