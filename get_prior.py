2cd13464bf52a2aad6fb7cafcd7117249d2fde29
Jesse Lew <jesse.lew@g.austincc.edu>
2018-04-26 19:27:32 -0400

    Added get_prior.py, fixed hainsworth styles

--- get_prior.py
+++ get_prior.py
@@ -1,8 +1,8 @@
 from scipy.io import loadmat
 import os
+import numpy as np
+import matplotlib.pyplot as plt
 import librosa
-import resampy
-from keras.preprocessing import sequence
 
 HAINSWORTH_STYLES = [
     'classical',
@@ -39,23 +39,25 @@ HAINSWORTH_SUBBEAT_STRUCTURES = [
 def load_hainsworth_annotations(filename):
     if not os.path.exists(filename):
         raise ValueError('{} does not exist'.format(filename))
-
+    
     datarec = loadmat(filename)['datarec']
     filename = datarec[0][0][0]
     artist = datarec[1][0][0]
     title = datarec[2][0][0]
     duration = float(datarec[3][0][0][0])
     style_num = int(datarec[4][0][0][0])
+    #print(style_num)
     tempo_oddity_num = int(datarec[5][0][0][0])
     difficulty_num = int(datarec[6][0][0][0])
     subbeat_struct_num = int(datarec[7][0][0][0])
     tempo = float(datarec[8][0][0][0])
     num_beats = int(datarec[9][0][0][0])
     beats = datarec[10][0].flatten()
+    
+    #assert len(beats) == num_beats
+    
 
-    assert len(beats) == num_beats
-
-    #y = beats[0].flatten()
+    y = beats[0].flatten()
     annotations = {
         'filename': filename,
         'artist': artist,
@@ -70,32 +72,45 @@ def load_hainsworth_annotations(filename):
     }
     return annotations
 
-def resample_hainsworth_beats(annotations, source_sr, target_sr):
-    annotations['beats'] = ((target_sr/source_sr) * annotations['beats']).astype(int)
+lags = []
+tempos = []
+label_dir = "./datasets/hainsworth/info/"
+sample_dir = "./datasets/hainsworth/samples/"
+for i in range(245):
+    # {} for replacement, : format spec, 03 pad up to 3 leading 0s, d is int
+    filename1 = "{:03d}_info.mat".format(i)
+    filename2 = "{:03d}.wav".format(i)
 
-# wrapper for hainsworth data
-def prep_hainsworth_data(data_dir, label_dir, target_sr=44100):
-    data_array = []
-    label_array = []
+    f1 = os.path.join(label_dir, filename1)
+    f2 = os.path.join(sample_dir, filename2)
+    if os.path.exists(f1) and os.path.exists(f2):
+        # Load audio at original sample rate
+        x, sr = librosa.load(f2, sr=None)
+        r = load_hainsworth_annotations(f1)
+        target_sr=44100
+        lag = 60*target_sr / (441 * r['tempo'])
+        tempos.append(r['tempo'])
+        lags.append(lag)
 
-    for i in range(2):
-        # {} for replacement, : format spec, 03 pad up to 3 leading 0s, d is int
-        filename1 = "{:03d}.wav".format(i)
-        filename2 = "{:03d}_info.mat".format(i)
+#print(rs)
+print(min(lags))
+print(max(lags))
+print(min(tempos))
+print(max(tempos))
 
-        f1 = os.path.join(data_dir, filename1)
-        f2 = os.path.join(label_dir, filename2)
-        if os.path.exists(f1) and os.path.exists(f2):
-            # Load audio at original sample rate
-            x, sr = librosa.load(f1, sr=None)
-            r = load_hainsworth_annotations(f2)
-            resample_hainsworth_beats(r, sr, target_sr)
+int_lags = [int(round(x)) for x in lags]
+print(int_lags)
+print(min(int_lags))
+print(max(int_lags))
 
-            # Resample audio to target sample rate
-            if target_sr != sr:
-                x = resampy.resample(x, sr, target_sr)
+min_lag = 20
+max_lag = 120
 
-            data_array.append(x)
-            label_array.append(r) # has tempos too
+hainsworth_prior_histogram = np.zeros((int(max_lag+1-min_lag),))
+#print(hainsworth_prior_histogram)
 
-    return data_array, label_array
+all_lags = list(range(min_lag, max_lag+1))
+for i in int_lags:
+    hainsworth_prior_histogram[i-min_lag] += 1
+print(hainsworth_prior_histogram)
+plt.bar(all_lags, hainsworth_prior_histogram)
