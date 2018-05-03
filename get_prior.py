622c3788d31263f9cfc08df80bc219d3ebc0a0a9
Jesse Lew <jesse.lew@g.austincc.edu>
2018-05-03 18:10:59 -0400

    Finished get_prior for Hainsworth

--- get_prior.py
+++ get_prior.py
@@ -1,116 +1,58 @@
-from scipy.io import loadmat
-import os
+from scipy.signal import gaussian, filtfilt #, lfilter
 import numpy as np
 import matplotlib.pyplot as plt
-import librosa
 
-HAINSWORTH_STYLES = [
-    'classical',
-    'solo classical',
-    'choral',
-    'rock/pop',
-    'dance',
-    'unused',
-    'jazz',
-    'big band jazz',
-    '60s pop',
-    'folk',
-    'random stuff'
-]
+def get_hainsworth_prior(r, sr, k=1, show=False)
+    """
+    Gets prior histogram of tempos from the Hainsworth dataset. 
+    """
 
-HAINSWORTH_TEMPO_ODDITIES = [
-    'normal',
-    'rall',
-    'sudden change',
-    'rubato'
-]
-
-HAINSWORTH_SUBBEAT_STRUCTURES = [
-    'not divided',
-    'quavers',
-    'semiquavers',
-    'demisemiquavers',
-    'triplets',
-    'into 6',
-    'swing',
-    'swung semiquavers'
-]
-
-def load_hainsworth_annotations(filename):
-    if not os.path.exists(filename):
-        raise ValueError('{} does not exist'.format(filename))
-    
-    datarec = loadmat(filename)['datarec']
-    filename = datarec[0][0][0]
-    artist = datarec[1][0][0]
-    title = datarec[2][0][0]
-    duration = float(datarec[3][0][0][0])
-    style_num = int(datarec[4][0][0][0])
-    #print(style_num)
-    tempo_oddity_num = int(datarec[5][0][0][0])
-    difficulty_num = int(datarec[6][0][0][0])
-    subbeat_struct_num = int(datarec[7][0][0][0])
-    tempo = float(datarec[8][0][0][0])
-    num_beats = int(datarec[9][0][0][0])
-    beats = datarec[10][0].flatten()
-    
-    #assert len(beats) == num_beats
-    
-
-    y = beats[0].flatten()
-    annotations = {
-        'filename': filename,
-        'artist': artist,
-        'title': title,
-        'duration': duration,
-        'style': HAINSWORTH_STYLES[style_num - 1],
-        'tempo_oddities': HAINSWORTH_TEMPO_ODDITIES[tempo_oddity_num - 1],
-        'difficulty': difficulty_num,
-        'subbeat_structure': HAINSWORTH_SUBBEAT_STRUCTURES[subbeat_struct_num - 1],
-        'tempo': tempo,
-        'beats': beats
-    }
-    return annotations
-
-lags = []
-tempos = []
-label_dir = "./datasets/hainsworth/info/"
-sample_dir = "./datasets/hainsworth/samples/"
-for i in range(245):
-    # {} for replacement, : format spec, 03 pad up to 3 leading 0s, d is int
-    filename1 = "{:03d}_info.mat".format(i)
-    filename2 = "{:03d}.wav".format(i)
-
-    f1 = os.path.join(label_dir, filename1)
-    f2 = os.path.join(sample_dir, filename2)
-    if os.path.exists(f1) and os.path.exists(f2):
-        # Load audio at original sample rate
-        x, sr = librosa.load(f2, sr=None)
-        r = load_hainsworth_annotations(f1)
+    lags = []
+    tempos = []
+    for i in range(len(r)):
         target_sr=44100
         lag = 60*target_sr / (441 * r['tempo'])
         tempos.append(r['tempo'])
         lags.append(lag)
 
-#print(rs)
-print(min(lags))
-print(max(lags))
-print(min(tempos))
-print(max(tempos))
+    # Create raw lag histogram
+    int_lags = [int(round(x)) for x in lags]
+    min_lag = 20
+    max_lag = 120
+    hainsworth_prior_histogram = np.zeros((int(max_lag+1-min_lag),))
+    all_lags = list(range(min_lag, max_lag+1))
+    for i in int_lags:
+        hainsworth_prior_histogram[i-min_lag] += 1
+    #plt.bar(all_lags, hainsworth_prior_histogram)
 
-int_lags = [int(round(x)) for x in lags]
-print(int_lags)
-print(min(int_lags))
-print(max(int_lags))
+    def add_k_smoothing(histo, k=1):  
+        newhisto = []
+        for i in range(len(histo)):
+            newhisto.append(histo[i] + k)
+        return newhisto
 
-min_lag = 20
-max_lag = 120
+    def gaussian_smoothing(histo):
+        standard_dev = np.std(histo)
+        lags = len(histo)
+        ghisto = gaussian(20, standard_dev/2)
+        ghisto /= ghisto.sum()
+        #print(max(ghisto))
+        #print(ghisto)
+        a = filtfilt(ghisto, [1], histo) 
+        return a
 
-hainsworth_prior_histogram = np.zeros((int(max_lag+1-min_lag),))
-#print(hainsworth_prior_histogram)
-
-all_lags = list(range(min_lag, max_lag+1))
-for i in int_lags:
-    hainsworth_prior_histogram[i-min_lag] += 1
-print(hainsworth_prior_histogram)
-plt.bar(all_lags, hainsworth_prior_histogram)
+    # Smooth the histogram
+    h1 = add_k_smoothing(hainsworth_prior_histogram, k)
+    h2 = gaussian_smoothing(hainsworth_prior_histogram)
+    h3 = gaussian_smoothing(h1)
+    
+    # Normalize and plot    
+    h1 /= sum(h1)
+    h2 /= sum(h2)
+    h3 /= sum(h3)
+    if show:
+        plt.bar(all_lags, h1, width=1)
+        plt.bar(all_lags, h2, width=1)
+        plt.bar(all_lags, h3, width=1)
+    
+    return h1, h2, h3
