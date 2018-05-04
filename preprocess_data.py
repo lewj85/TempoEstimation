e9fdb883ace9282b8ee7dbf7e3147a492c547d0d
Jason Cramer <jason.t.cramer@gmail.com>
2018-05-04 18:00:25 -0400

    Fix silly typo in preprocess_data

--- preprocess_data.py
+++ preprocess_data.py
@@ -12,7 +12,7 @@ def preprocess_data(audio_array, annotation_array, hop_size=441, mode='spectrogr
     if mode == 'spectrogram':
         # Compute spectrogram features
         X = [pad_sequences(feat)
-             for feat in zip(*[prep_spectrogram_features(x, sr=hop_size=hop_size)
+             for feat in zip(*[prep_spectrogram_features(x, sr=sr, hop_size=hop_size)
              for x in audio_array])]
         max_len = X[0].shape[1]
     elif mode == 'audio':
