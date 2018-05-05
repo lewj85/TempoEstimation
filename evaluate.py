c0e958b5fc832995613a15565ed2c2a60e4f3c6b
Jason Cramer <jason.t.cramer@gmail.com>
2018-05-05 13:35:51 -0400

    Add evaluation script and make model prediction batched smaller to avoid OOM

--- evaluate.py
+++ evaluate.py
@@ -1,4 +1,3 @@
-import datetime
 import json
 import os
 import numpy as np
@@ -6,10 +5,7 @@ import pickle as pk
 from argparse import ArgumentParser
 from ballroom import prep_ballroom_data
 from hainsworth import prep_hainsworth_data
-from preprocess_data import preprocess_data
-from model.training import train_model
 from evaluation import compute_beat_metrics, compute_tempo_metrics
-from data_utils import create_data_subsets
 from beat_tracking import estimate_beats_for_batch, \
     get_beat_times_from_annotations
 from tempo import get_tempos_from_annotations, estimate_tempos_for_batch
@@ -21,36 +17,14 @@ import logging
 LOGGER = logging.getLogger('tempo_estimation')
 LOGGER.setLevel(logging.DEBUG)
 
-HOP_SIZE =  0.01
-
-HAINSWORTH_MIN_TEMPO = 40
-HAINSWORTH_MAX_TEMPO = 250
+from train import HOP_SIZE, HAINSWORTH_MIN_TEMPO, HAINSWORTH_MAX_TEMPO
 
 def parse_arguments():
     """
     Get command line arguments
     """
-    parser = ArgumentParser(description='Train a deep beat tracker model')
-    parser.add_argument('data_dir', help='Path to audio directory')
-    parser.add_argument('label_dir', help='Path to annotation directory')
-    parser.add_argument('dataset', choices=['hainsworth', 'ballroom'],
-                        help='Name of dataset')
-    parser.add_argument('output_dir', help='Path where output will be saved')
-    parser.add_argument('--num-epochs', type=int, default=10,
-                        help='Number of epochs for training model')
-    parser.add_argument('--batch-size', type=int, default=5,
-                        help='Batch size for training')
-    parser.add_argument('--target-fs', type=int, default=44100,
-                        help='Target sample rate. If spectrogram samples is used, must be 44100')
-    parser.add_argument('--audio-window-size', type=int, default=2048,
-                        help='Audio window size')
-    parser.add_argument('--lr', type=float, default=0.001,
-                        help='Learning rate')
-    # -m is shortcut for --model
-    parser.add_argument('--model', '-m', dest='model_type', default='spectrogram',
-                        choices=['spectrogram', 'audio'], help='Model type')
-    # TODO: Add command line arguments for training hyperparameters
-
+    parser = ArgumentParser(description='Evaluate a deep beat tracker model')
+    parser.add_argument('model_dir', help='Path to model directory')
 
     # Creates a namespace object
     args = parser.parse_args()
@@ -59,95 +33,45 @@ def parse_arguments():
 
 
 # CHANGE SAMPLE RATE TO 24kHz or 16kHz
-def main(data_dir, label_dir, dataset, output_dir, num_epochs=10, batch_size=5,
-         lr=0.001, target_fs=44100, audio_window_size=2048, model_type='spectrogram'):
+def main(model_dir):
     """
     Train a deep beat tracker model
     """
     # Set up logger
     init_console_logger(LOGGER, verbose=True)
 
-    output_dir = os.path.join(output_dir, model_type, dataset)
-    LOGGER.info('Output will be saved to {}'.format(output_dir))
-
-    feature_data_dir = os.path.join(output_dir, 'data')
-    model_dir = os.path.join(output_dir, 'model',
-                             datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
-    if not os.path.exists(feature_data_dir):
-        os.makedirs(feature_data_dir)
-    if not os.path.exists(model_dir):
-        os.makedirs(model_dir)
-
-    LOGGER.info('Saving configuration.')
-    config = {
-        'data_dir': data_dir,
-        'label_dir': label_dir,
-        'dataset': dataset,
-        'output_dir': output_dir,
-        'num_epochs': num_epochs,
-        'batch_size': batch_size,
-        'lr': lr,
-        'target_fs': target_fs,
-        'audio_window_size': audio_window_size,
-        'model_type': model_type
-    }
     config_path = os.path.join(model_dir, 'config.json')
-    with open(config_path, 'w') as f:
-        json.dump(config, f)
+
+    with open(config_path, 'r') as f:
+        config = json.load(f)
+
+    data_dir = config['data_dir']
+    label_dir = config['label_dir']
+    dataset = config['dataset']
+    output_dir = config['output_dir']
+    num_epochs = config['num_epochs']
+    batch_size = config['batch_size']
+    lr = config['lr']
+    target_fs = config['target_fs']
+    audio_window_size = config['audio_window_size']
+    model_type = config['model_type']
+    feature_data_dir = os.path.join(output_dir, 'data')
+    hop_length = int(target_fs * HOP_SIZE)
 
     LOGGER.info('Loading {} data.'.format(dataset))
     train_data_path = os.path.join(feature_data_dir, '{}_train_data.npz').format(dataset)
     valid_data_path = os.path.join(feature_data_dir, '{}_valid_data.npz').format(dataset)
     test_data_path = os.path.join(feature_data_dir, '{}_test_data.npz').format(dataset)
 
-    data_exists = os.path.exists(train_data_path) \
-        and os.path.exists(valid_data_path) \
-        and os.path.exists(test_data_path)
-
-    if model_type == 'spectrogram':
-        assert target_fs == 44100
-
-    hop_length = int(target_fs * HOP_SIZE)
-
-    # Load audio and annotations
     if dataset == 'hainsworth':
         a, r = prep_hainsworth_data(data_dir, label_dir, target_fs,
-                                    load_audio=not data_exists)
+                                    load_audio=False)
     elif dataset == 'ballroom':
         a, r = prep_ballroom_data(data_dir, label_dir, hop_length, target_fs,
-                                  load_audio=not data_exists)
-
-    if not data_exists:
-        # Create preprocessed data if it doesn't exist
-        LOGGER.info('Preprocessing data for model type "{}".'.format(model_type))
-        # Get features and targets from data
-        X, y = preprocess_data(a, r, mode=model_type, hop_size=hop_length,
-                               audio_window_size=audio_window_size, sr=target_fs)
-
-        LOGGER.info('Creating data subsets.')
-        train_data, valid_data, test_data = create_data_subsets(X, y)
-
-        LOGGER.info('Saving data subsets to disk.')
-        np.savez(train_data_path, **train_data)
-        np.savez(valid_data_path, **valid_data)
-        np.savez(test_data_path, **test_data)
-
-    else:
-        # Otherwise, just load existing data
-        train_data = np.load(train_data_path)
-        valid_data = np.load(valid_data_path)
-        test_data = np.load(test_data_path)
-
-    model_path = os.path.join(model_dir, 'model.hdf5')
-    if not os.path.exists(model_path):
-        # Only train model if we haven't done so already
-        LOGGER.info('Training model.')
-        # Create, train, and save model
-        model_path = train_model(train_data, valid_data, model_type, model_path,
-                                 lr=lr, batch_size=batch_size, num_epochs=num_epochs,
-                                 audio_window_size=audio_window_size)
+                                  load_audio=False)
 
     LOGGER.info('Loading model.')
+    model_path = os.path.join(model_dir, 'model.hdf5')
     model = load_model(model_path)
 
     frame_rate = target_fs / hop_length
